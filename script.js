af6333b3ece784dbbd46bcc9b120abe41537b34a
MikeWhalenIII <michael1327@hotmail.com>
2020-05-10 09:21:02 -0400

    Disabled bar transitions and updated buttons

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function resetArray() {
 
     for (let index = 0; index < array.length; index++) {
         data += '<div class="progress bg-transparent" style="margin-bottom: 2px">' +
-            '<div class="progress-bar" id="' + index + '" role="progressbar" style="width: ' + array[index] + '% " aria-valuenow="' + array[index] + '" aria-valuemin="0" aria-valuemax="40">' + array[index] + '</div></div>';
+            '<div class="progress-bar" id="' + index + '" role="progressbar" style="width: ' + array[index] + '%; transition:none; " aria-valuenow="' + array[index] + '" aria-valuemin="0" aria-valuemax="40">' + array[index] + '</div></div>';
     }
     document.getElementById("arrayBars").innerHTML = data;
 }
@@ -19,18 +19,21 @@ async function bubbleSort() {
     var outer = "", inner = "";
     var nElems = array.length;
 
+    // Disable buttons during sort
+    $("#buttons").children().prop('disabled', true);
+
     for (outer = nElems - 1; outer > 1; outer--) {
         for (inner = 0; inner < outer; inner++) {
             // Change the color of the two bars being compared to yellow
             document.getElementById(inner).className = "progress-bar bg-warning";
             document.getElementById(inner + 1).className = "progress-bar bg-warning";
-            await sleep(50);
+            await sleep(10);
             
             if (array[inner] > array[inner + 1]) {
                 swap(inner, inner + 1, outer);
                 document.getElementById(inner).className = "progress-bar bg-danger";
                 document.getElementById(inner + 1).className = "progress-bar bg-danger";
-                await sleep(50);
+                await sleep(10);
             }
             // Change the two bars back to blue
             document.getElementById(inner).className = "progress-bar";
@@ -43,6 +46,8 @@ async function bubbleSort() {
             }
         }
     }
+    // Enable buttons once sorting has been completed
+    $("#buttons").children().prop('disabled', false);
 }
 
 function swap(one, two, outer) {
